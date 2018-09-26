9895b54e4bafcf399e7518ff4c396b3e37437649
Zach Wight <zacharywight@gmail.com>
2018-09-26 14:52:12 -0600

    updates

---
+++ liri.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+module.exports = {
+twitter: {
+  
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  
+},
+spotify: {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+}
+//spotfiy 
+};
